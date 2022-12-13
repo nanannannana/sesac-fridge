ea61f61ad6d0ae2dfefd5b9474b37c06626f0649
nietzche15 <yeongeun93@gmail.com>
2022-12-13 20:31:37 +0900

    feat:myFridge page 코드 작성 commit

--- controller/fridge/myFridge.js
+++ controller/fridge/myFridge.js
@@ -1,11 +1,19 @@
+const { Fresh } = require("../../model/fresh");
+const { Frozen } = require("../../model/frozen");
+const { Recipe } = require("../../model/recipe"); 
+
 
 // 나의 냉장고 페이지 렌더 - 영은
 exports.getMyFridge = (req,res) => {
     res.render("fridge/myFridge");
 }
 
-exports.postResultRecipe = (req,res) => {
-    // exports.postResultRecipe = async (req,res) => {
+exports.postResultRecipe = async (req,res) => {
     console.log( "req.body.checkedIngdList : ", req.body.checkedIngdList );
-    res.send(true);
+    let result = await Recipe.findAll({
+        attributes : [ "recipe_title" ],
+        where : { recipe_ingd : req.body.checkedIngdList[0] }
+    }); 
+    console.log("postResultRecipe result : ", result );
+    res.send( result );
 }
