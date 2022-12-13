2e0da7bafa3f09f445f405952836ca093dfaeb0a
nanannannana <rhaehfdl032037@gmail.com>
2022-12-13 19:22:50 +0900

    feat:yeji css commit

--- controller/user/myPage.js
+++ controller/user/myPage.js
@@ -1,4 +1,7 @@
 // 마이 페이지 렌더 - 예지
-exports.getMyPage = function(req,res) {
-    res.render("user/myPage");
+exports.getMyProfile = function(req,res) {
+    res.render("user/myProfile");
+}
+exports.getWishList = function(req,res) {
+    res.render("user/wishList");
 }
