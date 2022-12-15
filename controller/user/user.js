2485813e40655ffc1ec177b3458c389f5237fb26
nanannannana <rhaehfdl032037@gmail.com>
2022-12-15 21:21:03 +0900

    feat: css 및 회원가입 유효성 검사 완료 commit

--- controller/user/user.js
+++ controller/user/user.js
@@ -1,17 +1,44 @@
+const { user } = require("../../model/");
+
 // 로그인 페이지 렌더
 exports.getSignin = function(req,res) {
     res.render("user/signIn");
 }
-exports.postSignin = function(req,res) {
-    console.log(req.body);
-    res.send(true);
+exports.postSigninFlag = async function(req,res) {
+    let result = await user.findAll({where:{user_id:req.body.user_id}});
+    if (result.length>0) res.send(true)
+    else res.send(false);
 }
 
 // 회원가입 페이지 렌더
 exports.getSignup = function(req,res) {
     res.render("user/signUp");
 }
-exports.postSignup = function(req,res) {
-    console.log(req.body);
-    res.send(true);
+
+// 아이디 중복 확인
+exports.postIdCheck = async function(req,res) {
+    // let data = {user_id: req.body.user_id};
+    let result = await user.findAll({where:{user_id: req.body.user_id}});
+    if (result.length>0) res.send(false)
+    else res.send(true);
+}
+
+// 회원가입 완료
+exports.postSignupFlag = async function(req,res) {
+    if (req.body.false) res.send("none")
+    else if (req.body.user_id) {
+        let result = await user.findAll({where:{user_id: req.body.user_id}});
+        let data = {
+            user_id: req.body.user_id,
+            user_pw: req.body.user_pw,
+            user_name: req.body.user_name,
+            user_phone: req.body.user_phone
+        }
+        if (result.length>0) {
+            res.send(false);
+        } else {
+            await user.create(data);
+            res.send(true);
+        }
+    }
 }
