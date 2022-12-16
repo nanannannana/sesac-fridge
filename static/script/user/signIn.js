fb2f841a6935ec878aaa714063cc9ff0acf69738
nanannannana <rhaehfdl032037@gmail.com>
2022-12-16 23:13:48 +0900

    feat: yeji commit

--- static/script/user/signIn.js
+++ static/script/user/signIn.js
@@ -4,23 +4,14 @@ function signin() {
         user_id: form.user_id.value,
         user_pw: form.user_pw.value
     };
-    if (form.user_id.value == "") {
+    if (form.user_id.value == "" || form.user_pw.value == "") {
         axios({
             method: "post",
             url: "/signinFlag",
-            data: {flag_false: "false"}
+            data: {signin_flag: "false"}
         })
         .then(function() {
-            swal("아이디 빈칸 안 됨", "빈칸 노", "warning");
-        })
-    } else if (form.user_pw.value == "") {
-        axios({
-            method: "post",
-            url: "/signinFlag",
-            data: {flag_false: "false"}
-        })
-        .then(function() {
-            swal("비밀번호 빈칸 안 됨", "빈칸 노", "warning");
+            swal("이메일 또는 비밀번호를 입력해주세요.","", "warning");
         })
     } else {
         axios({
@@ -28,9 +19,14 @@ function signin() {
             url: "/signinFlag",
             data: data
         })
-        .then(async function() {
-            await swal("Good job!", "You clicked the button!", "success");
-            location.href="/";
+        .then(async function(res) {
+            if (res.data) {
+                await swal("로그인에 성공하였습니다!", "버튼을 누르면 메인페이지로 이동합니다", "success");
+                location.href="/";
+            } else {
+                await swal("로그인에 실패하였습니다!", "아이디 또는 비밀번호를 다시 입력해주세요.", "error");
+            }
+
         })
     }
 
