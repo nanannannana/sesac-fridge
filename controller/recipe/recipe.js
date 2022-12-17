225f634d3b4453a3f3b7204e3742f7b6e26c4b36
jihyang <jihyangi043@gmail.com>
2022-12-17 19:01:07 +0900

    feat: recipe 페이지 db 연결 및 레이아웃

--- controller/recipe/recipe.js
+++ controller/recipe/recipe.js
@@ -4,8 +4,12 @@ const { recipe } = require("../../model");
 //     res.render("recipe/recipe");
 // }
 
+// 레시피 추천 페이지
 exports.getRecipe = async (req, res) => {
-    let result = await recipe.findAll(); 
+    let result = await recipe.findAll({
+        raw : true, // dataValues만 가져오기
+        where : { recipe_tag : null }
+    }); 
     console.log(result[0]);
     res.render("recipe/recipe", {data : result[0]});
 }
