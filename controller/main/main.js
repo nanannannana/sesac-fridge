ae315a0e3874422950e183fee688a0dc11518472
jihyang <jihyangi043@gmail.com>
2022-12-19 15:39:12 +0900

    header 정리

--- controller/main/main.js
+++ controller/main/main.js
@@ -5,12 +5,12 @@ const { recipe } = require("../../model");
 // 메인 페이지 렌더 - 주안
 exports.getMain = async (req,res) => {
 
-    if(req.session.user){let log_time=new Date(today.setDate(today.getDate() + 2));}
+    // if(req.session.user){let log_time=new Date(today.setDate(today.getDate() + 2));}
 
     // let result = await fresh.findAndCountAll({
     //     where : {
 
     //     }
     // })
-    res.render("/main/main");
+    res.render("main/main");
 }
