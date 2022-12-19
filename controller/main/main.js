5dccc372d647b6f4ae8b0f805f9631624f4c01fd
nietzche15 <yeongeun93@gmail.com>
2022-12-20 04:15:47 +0900

    feat:메인 알림

--- controller/main/main.js
+++ controller/main/main.js
@@ -4,18 +4,21 @@ const { recipe } = require("../../model");
 const { user } = require("../../model");
 const { Op } = require("sequelize");
 
-// 메인 페이지 렌더 - 주안
-exports.getMain = async (req,res) => {
-    var exp_date = new Date();
-    exp_date.setHours(23,59,59);
-    console.log("exp_date : ",exp_date);
-
+//global variable
+//로그인 시각 기준으로 시간 set
     let today = new Date();
-    var date = new Date(); //date : 이틀 후
-    date.setDate( date.getDate()+2 ); 
-    console.log( "date: ",  date );
-    // 로그인 한 경우,
    
+    let exp_date = new Date(); 
+    exp_date.setHours(23,59,59);
+    // console.log("exp_date : ",exp_date);
+
+    let date = new Date(); //date : 이틀 후
+    date.setDate( date.getDate()+2 ); 
+    // console.log( "date: ",  date );
+
+// 메인 페이지 렌더 - 영은
+exports.getMain = async (req,res) => {
+    // 로그인 한 경우,
     // 임박 식재료 개수
         let fresh_count = await fresh.findAndCountAll({
             where: {
@@ -25,47 +28,63 @@ exports.getMain = async (req,res) => {
                 }
             },
         })
+
     // 유통기한 지난 식재료 개수 & list
         let exp_list = await fresh.findAndCountAll({
             where : {
                 fresh_expire : {
-                    [Op.lt] : date
+                    [Op.lt] : exp_date
                 }
             }
         })
-        console.log("log fresh_count :", fresh_count.count );
-        console.log("log exp_list :", exp_list.count );
+        // console.log("log fresh_count :", fresh_count.count );
+        // console.log("log exp_list :", exp_list.rows );
+    
+    //로그인한 경우 session에서 user name
+    // & cookie 에서 EXP_MODAL value 확인
+    
     // user name
     if(req.session.user){ 
         let user_name = await user.findOne({
             attributes : ["user_name"],
             where : {user_id : req.session.user}
         });
+    console.log("log user_name : ", user_name.user_name );
 
-
-    console.log("log user_id : ", user_name );
-
-        res.render("main/main", { 
-            isLogin : true, 
-            fresh_count : fresh_count.count,
-            exp_count : exp_list.count,
-            user_name : user_name
-        }); 
+        if(req.cookies.EXP_MODAL==1){
+            res.render("main/main", {  // 로그인 O & 모달 오늘안봄 O
+                isLogin : true, 
+                fresh_count : fresh_count.count,
+                exp_count : exp_list.count,
+                user_name : user_name.user_name,
+                exp_modal : true 
+            }); 
+        }else{
+            res.render("main/main",{ //로그인 O & 모달 오늘안봄 X
+                    isLogin : true, 
+                    fresh_count : fresh_count.count,
+                    exp_count : exp_list.count,
+                    user_name : user_name.user_name,
+                    exp_modal : false
+            })
+        }        
     }
-    else{ res.render("main/main", { 
+    else{ res.render("main/main", { //로그인 X
         isLogin : false, 
         fresh_count : false,
         exp_count : false,
-        user_name : false
+        user_name : false,
+        exp_modal : false
     });  }
 }
 
-exports.postCookie = (req, res) => {
+// 식재료 삭제 알림 modal cookie 생성
+exports.postModalCookie = (req, res) => {
     res.cookie("EXP_MODAL","1", {
         httpOnly : true,
         expires : exp_date,
-        secure : false
     });
+    res.send(true);
 }
 
 
--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -2,10 +2,10 @@ const express = require("express");
 const main = require("../controller/main/main");
 const router = express.Router();
 
-// 메인 페이지 렌더 - 주안
+// 메인 페이지 렌더 - 영은
 router.get("/", main.getMain);
 // 쿠키 생성
-router.post("/postCookie", main.postCookie);
+router.post("/modalCookie", main.postModalCookie);
 
 
 
