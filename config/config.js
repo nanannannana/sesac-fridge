8e7ca71862393e3bae147e53e3ba9ed2acacf5e5
nietzche15 <yeongeun93@gmail.com>
2022-12-22 18:47:09 +0900

    feat:username 수정

--- config/config.js
+++ config/config.js
@@ -1,19 +1,16 @@
 const env = process.env;
-
 const development = {
     "host" : "localhost",
     "database" : env.DB,
-    "username" : env.USER,
-    "password" : env.PASSWORD, 
+    "username" :  env.DB_USER,
+    "password" : env.DB_PASSWORD,
     "dialect" : "mysql"
 }
-
 const production = {
         "host" : env.PUBLIC_IP,
         "database" : env.DB,
-        "username" : env.USER,
-        "password" : env.PASSWORD, 
+        "username" :  env.DB_USER,
+        "password" : env.DB_PASSWORD,
         "dialect" : "mysql"
 }
-
 module.exports = {development, production}
