0b6617f32d2b28f8180937bb36c444530b9c97a5
juuuuuan <jooan28@gmail.com>
2022-12-22 14:47:46 +0900

    feat: 주안 test commit

--- app.js
+++ app.js
@@ -55,6 +55,24 @@ app.get("*", (req,res)=>{
 // ConnectDB();
 
 
+// DB 연결 성공 여부
+// const {sequelize} = require('./model/index'); 
+// // 다른 require문은 일단 생략
+// const ConnectDB = async () => {
+//     try {
+//         await sequelize.authenticate().then( 
+//             () => console.log('데이터베이스 연결 성공!')
+//         );
+//         await sequelize.sync().then(
+//             () => console.log('동기화 완료!')
+//         );
+//     } catch (error) {
+//         console.error('DB 연결 및 동기화 실패', error);
+//     }
+// }
+// // DB와 연결 및 동기화
+// ConnectDB();
+
 app.listen(process.env.PORT, ()=>{
     console.log("server open : ", process.env.PORT);
 })
