b8b06b1c86d7052df0c6b674ae8698e732b8b552
daisy <gid5214@naver.com>
2022-12-24 11:46:05 +0900

    feat: 빠른 한끼 btn

--- routes/recipeRoute.js
+++ routes/recipeRoute.js
@@ -9,7 +9,6 @@ router.get("/", recipe.getRecipe);
 // 요리하기 버튼 추가 시 fridge에 update할 정보
 router.patch("/toFridge", recipe.patchToFridge);
 
-
 // 최근 본 레시피 (log 테이블에 저장) 
 router.post("/insertToLog", recipe.postInsertToLog);
 // 좋아요 (recipe_like 테이블에 저장)
