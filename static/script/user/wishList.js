90eb22af99ba602d312d3200539b1dd9db29f17b
nanannannana <rhaehfdl032037@gmail.com>
2022-12-28 05:18:49 +0900

    feat: 로그인, 로그아웃, 마이ㅔ이지, 메인 반응형 수정 commit

--- static/script/user/wishList.js
+++ static/script/user/wishList.js
@@ -9,21 +9,30 @@ function wishlist_del(recipe_id, target) {
         $("#heart_icon").addClass("bi-heart");
         $("#wishlist_card_box_child").remove();
         $("#wishlist_card_box").append(`<div id="wishlist_card_box_child"></div>`);
-        for (var i=0; i<res.data.recipe_id.length;i++) {
+        if (res.data.recipe_id.length == 0) {
             $("#wishlist_card_box_child").append(`
-                    <div id="card_size" class="card mb-4 me-3">
-                        <img src="${res.data.recipe_img[i]}" class="card-img" alt="...">
-                        <div id="card_hover" class="card-img-overlay">
-                            <i id="heart_icon" class="bi bi-heart-fill" type="button" onclick="wishlist_del(${res.data.recipe_id[i]}, this)"></i>
-                            <form id="form_hidden">
-                                <input type="hidden" name="recipe_id" value="${res.data.recipe_id[i]}">
-                            </form>
-                            <div id="card_text_box">
-                                <h1 id="card_text" class="card-title">${res.data.recipe_title[i]}</h1>
-                                <h5 id="card_text" class="card-text" type="button" onclick="location.href='${res.data.recipe_url[i]}'" target="_blank">레시피 보기</h5>
-                            </div>
+            <div id="none_box">
+                <img src="/static/img/wishlist_none.png">
+            </div>
+            `)
+        } else {
+            for (var i=0; i<res.data.recipe_id.length;i++) {
+                $("#wishlist_card_box_child").append(`
+                <div id="card_size" class="card mb-4 me-3">
+                    <img src="${res.data.recipe_img[i]}" class="card-img" alt="...">
+                    <div id="card_hover" class="card-img-overlay">
+                        <i id="heart_icon" class="bi bi-heart-fill" type="button" onclick="wishlist_del(${res.data.recipe_id[i]}, this)"></i>
+                        <form id="form_hidden">
+                            <input type="hidden" name="recipe_id" value="${res.data.recipe_id[i]}">
+                        </form>
+                        <div id="card_text_box">
+                            <h1 id="card_text" class="card-title">${res.data.recipe_title[i]}</h1>
+                            <h5 id="card_text" class="card-text" type="button" onclick="location.href='${res.data.recipe_url[i]}'" target="_blank">레시피 보기</h5>
                         </div>
                     </div>
-        `)};
+                </div>
+                `)
+            };
+        }
     })
 }
