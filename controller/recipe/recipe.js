dbfc41da8e64a2237d78678d96aa7c0696abee8e
jihyang <jihyangi043@gmail.com>
2022-12-27 17:24:16 +0900

    좋아요 버튼 구현

--- controller/recipe/recipe.js
+++ controller/recipe/recipe.js
@@ -31,6 +31,16 @@ exports.getRecipe = async (req, res) => {
             raw : true,
             attributes : [['user_user_id', "userId"], ['recipe_recipe_id', 'recipeId'], ['like_id', 'likeId']],
         })
+        // console.log("likeUser", likeUser);
+        // console.log("likeUser userId", likeUser[0].userId);
+        // likeUser [
+        //     { userId: 'jsesac@naver.com', recipeId: 1248, likeId: 136 },
+        //     { userId: 'jsesac@naver.com', recipeId: 1352, likeId: 137 },
+        //     { userId: 'jsesac@naver.com', recipeId: 1235, likeId: 138 },
+        //     { userId: 'rhaehfdl0320@naver.com', recipeId: 1237, likeId: 140 },
+        //     { userId: 'rhaehfdl0320@naver.com', recipeId: 1238, likeId: 141 },
+        //     { userId: 'hello12@naver.com', recipeId: 1233, likeId: 146 }
+        // ]
 
         // [1]-2 fresh, frozen 테이블에서 검색한 결과를 합쳐서 ingdRes에 넣는다.
         let ingdRes = [];  
@@ -60,19 +70,20 @@ exports.getRecipe = async (req, res) => {
         // 식재료가 있을 때 일치하는 식재료가 있으면 보여준다.
         let where = {}; // 레시피에서 검색할 때 사용할 where 절
 
-        // [3]-1 식재료가 있을 때
+        // [2]-1 식재료가 있을 때
         if(ingdRes.length > 0){ 
             let result;
             let recipes;
-            // [3]-1-1 검색어로 렌더
+            // [2]-1-1 검색어로 렌더
             if(req.query.keyword){
                 console.log("키워드렌더입니다.")
+                console.log("키워드", req.query.keyword)
                 recipes = await recipe.findAll({
                     raw : true,
                     where : { ["recipe_ingd"] : { [Op.regexp] : req.query.keyword}}
                 })
             }
-            // [3]-1-2 빠른한끼 태그로 렌더
+            // [2]-1-2 빠른한끼 태그로 렌더
             if(req.query.tag == "빠른한끼") {
                 console.log("빠른한끼렌더입니다.");
                 recipes = await recipe.findAll({
@@ -84,6 +95,7 @@ exports.getRecipe = async (req, res) => {
                     limit : 45
                 })
             }
+            // [2]-1-3 빠른한끼 태그로 렌더
             if(req.query.tag === "식재료일치") {
                 console.log("식재료 일치렌더입니다.")
                 recipes = await recipe.findAll({
@@ -91,7 +103,7 @@ exports.getRecipe = async (req, res) => {
                     where : {["recipe_ingd"] : {[Op.regexp] : ingdNameStr }},
                 })
             }
-            // [3]-1-3 기본 렌더
+            // [2]-1-4 기본 렌더
             if(req.query.tag != "빠른한끼" && req.query.tag != "식재료일치" && !req.query.keyword ) { 
                 console.log("기본렌더입니다.")
                 // 식재료랑 비슷하게 일치하는 레시피가 있을 때,
@@ -105,8 +117,9 @@ exports.getRecipe = async (req, res) => {
                 });
                 
             }
-            result = { data: recipes }; 
-            // [3]-1-4 식재료가 있을 때 프론트 단에서 사용할 나의 재료 이름과 수량
+            result = { data: recipes, dataLike : likeUser }; 
+            console.log("dataLike: ",result.dataLike);
+            // [2]-1-5 식재료가 있을 때 프론트 단에서 사용할 나의 재료 이름과 수량
             let ingdResult = []; 
             for(var i=0; i<recipes.length;i++) {
                 ingdResult.push(recipes[i].recipe_ingd);
@@ -116,16 +129,17 @@ exports.getRecipe = async (req, res) => {
             result["ingdRange"] = ingdRange;
             result["ingdResult"] = ingdResult;
             result["isLogin"] = true;
-            result["haveIngd"] = true;
+            result["user_id"] = final_user_id;
             if(req.query.tag) {
                 result["tag"] = req.query.tag;
             }else { 
                 result["tag"] = "유사 재료 레시피";
             }
             res.render("recipe/recipe", result);
-        } else { // [5] 로그인 했는데 식재료가 없을 때(냉장고 빈 것 포함)
+        } else { // [2]-2 로그인 했는데 식재료가 없을 때(냉장고 빈 것 포함)
             let result;
             let recipes;
+            // [2]-2-1 로그인 시 식재료 없을 때 기본 렌더
             if(req.query.tag != "빠른한끼" && !req.query.keyword) { 
                 console.log("로그인, 식재료 없는 기본 렌더");
                 let where ={};
@@ -141,7 +155,7 @@ exports.getRecipe = async (req, res) => {
                     where
                 });
             }
-            // 로그인 시 식재료 없을 때 빠른 한끼 렌더
+            // [2]-2-2 로그인 시 식재료 없을 때 빠른 한끼 렌더
             if(req.query.tag == "빠른한끼") { 
                 console.log("로그인, 식재료 없는 빠른 한끼 태그")
                 recipes = await recipe.findAll({
@@ -150,7 +164,7 @@ exports.getRecipe = async (req, res) => {
                     limit : 45
                 })
             } 
-            // 비로그인 시 검색어로 렌더
+            // [2]-2-3 로그인 시 식재료 없을 때 검색어로 렌더
             if(req.query.keyword) { 
                 console.log("로그인, 식재료 없는 검색어 렌더")
                 recipes = await recipe.findAll({
@@ -168,8 +182,8 @@ exports.getRecipe = async (req, res) => {
             res.render("recipe/recipe_non", result);
         }
         
-    }else { // [5] 비로그인 시 
-        // 비로그인 시 기본 렌더
+    }else { // [3] 비로그인 시 
+        // [3]-1 비로그인 시 기본 렌더
         let result;
         let recipes;
         if(req.query.tag != "빠른한끼" && !req.query.keyword) { 
@@ -187,7 +201,7 @@ exports.getRecipe = async (req, res) => {
                 where
             });
         }
-         // 비로그인 시 빠른 한끼 렌더
+        // [3]-2 비로그인 시 빠른 한끼 렌더
         if(req.query.tag == "빠른한끼") { 
             console.log("비로그인시 빠른 한끼 태그")
             recipes = await recipe.findAll({
@@ -196,7 +210,7 @@ exports.getRecipe = async (req, res) => {
                 limit : 45
             })
         } 
-        // 비로그인 시 검색어로 렌더
+        // [3]-3 비로그인 시 검색어로 렌더
         if(req.query.keyword) { 
             console.log("비로그인시 검색어 렌더")
             recipes = await recipe.findAll({
@@ -413,20 +427,23 @@ exports.postInsertToLike = async (req,res) => {
 
     const final_user_id = (req.cookies.user_id === undefined) ? req.session.user : req.cookies.user_id;
     // recipe tb에 컬럼 수정
-    await recipe.update(
+    let result = await recipe.update(
         {recipe_pick : 1},
         {where : { recipe_id : req.body.id }}
     )  
-    // 같은 레시피 id가 존재하면 recipe_like DB에 create 하지 않음
-    let [find, create]  = await recipe_like.findOrCreate({
-        where : { recipe_recipe_id : req.body.id },
+
+    // 같은 레시피 id와 유저가 존재하면 recipe_like DB에 create 하지 않음
+    let [find, create] = await recipe_like.findOrCreate({
+        where : { recipe_recipe_id : req.body.id, user_user_id : final_user_id },
         defaults : {
             recipe_recipe_id : req.body.id,
             user_user_id : final_user_id,
         }
     })
-    likeUser = find.user_user_id;
-    // find에서 create 하지 못해도 true 넘기고, create해도 true
-    res.send(find.user_user_id);
+    if(create) { // 생성 시 
+        res.send(create);
+    }else {
+        res.send(find);
+    }
 }
 
