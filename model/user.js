d740ae5c4692b4da676f0169727698608a41f5d7
jihyang <jihyangi043@gmail.com>
2023-02-28 18:00:33 +0900

    feat: 로그인 버그 수정

--- model/user.js
+++ model/user.js
@@ -1,40 +1,35 @@
 const user = (Sequelize, DataTypes) => {
-    return Sequelize.define(
-        "user",
-        {
-            user_id : {
-                type : DataTypes.STRING(40),
-                allowNull : false,
-                primaryKey : true
-            }, 
-            user_pw : {
-                type : DataTypes.STRING(20),
-                allowNull : false
-            },
-            user_name : {
-                type : DataTypes.STRING(20),
-                allowNull : false
-            },
-            user_phone : {
-                type : DataTypes.INTEGER,
-                allowNull : false
-            },
-            kakao_login : {
-                type : DataTypes.INTEGER,
-                allowNull: false,
-                defaultValue : 0
-            }
-        },
-        {
-            charset: 'utf8mb4',
-            collate: 'utf8mb4_unicode_ci',
-            underscored: true, // 카멜표기법-> 스네이크로
-            tableName : "user",
-            freezeTableName : true,
-            timestamps : false,
-            paranoid: false, // true : deletedAt이라는 컬럼이 생기고 지운 시각이 기록
-        }
-    )
-}
+  return Sequelize.define(
+    "user",
+    {
+      user_id: {
+        type: DataTypes.STRING(40),
+        allowNull: false,
+        primaryKey: true,
+      },
+      user_pw: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      user_name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      user_phone: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      charset: "utf8mb4",
+      collate: "utf8mb4_unicode_ci",
+      underscored: true, // 카멜표기법-> 스네이크로
+      tableName: "user",
+      freezeTableName: true,
+      timestamps: false,
+      paranoid: false, // true : deletedAt이라는 컬럼이 생기고 지운 시각이 기록
+    }
+  );
+};
 
 module.exports = user;
